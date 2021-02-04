0cefc25d989e71114ff04badcde28851a73a7805
James Masaviru <jmasaviru@gmail.com>
2021-02-04 05:32:58 -0500

    Updating schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL UNSIGNED NOT NULL,
+    salary DECIMAL(10,4) UNSIGNED NOT NULL,
     department_id INT UNSIGNED NOT NULL,
     INDEX dept_index (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
