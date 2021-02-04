055894637beb3ab0788c56ac74a7bdc3d14ef7a6
James Masaviru <jmasaviru@gmail.com>
2021-02-04 18:26:41 -0500

    Updating connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,17 +5,20 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     // Host for Development
     host: "localhost",
-    // Port for Development Database
+    // Port for Development of Database
     port: 3306,
     // Database user-name
     user: "root",
     // Database password
-    password: "rootroot",
+    password: "Ma$terviru6",
     // Database Name
     database: "Employees"
 });
 // Connect to the Database
-connection.connect();
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
 
 // Setting up the connection to use promises instead of callbacks
 connection.query = util.promisify(connection.query);
