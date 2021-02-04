0d0cb274cdc98396d181fd7b9bb8657a54816830
James Masaviru <jmasaviru@gmail.com>
2021-02-03 21:46:52 -0500

    Updating connection.js

---
+++ db/connection.js
@@ -0,0 +1,25 @@
+const util = require("util");
+const mysql = require("mysql");
+const { createConnection } = require("net");
+
+// Create connection
+const connection = mysql.createConnection({
+    // Host for Development
+    host: "localhost",
+    // Port for Development Database
+    port: 3306,
+    // Database user-name
+    user: "root",
+    // Database password
+    password: "rootroot",
+    // Database Name
+    database: "Employees"
+});
+// Connect to the Database
+connection.connect();
+
+// Setting up the connection to use promises instead of callbacks
+connection.query = util.promisify(connection.query);
+
+// Exporting the Database Connection
+module.exports = connection;
